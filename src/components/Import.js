e30ad074465590226b71b4776bb7b70dfc541558
heydonhatcher <heyhatch@gmail.com>
2020-03-02 14:27:40 -0600

    got cars listed out

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Container
+} from "@material-ui/core";
 
-const Import = (props) => {
-    // fill out this component
+const Import = props => {
+  return (
+    <div>
+      <Container>
+        <h2 style={{ color: "blue" }}>COUNT: {props.makes.length}</h2>
+        <Button onClick={props.fetchMakes} variant="contained" color="primary">
+          Import
+        </Button>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Id</TableCell>
+              <TableCell align="right">Make/Model</TableCell>
+              <TableCell align="right">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.makes.map((make, index) => {
+              return (
+                <TableRow key={make.MakeId}>
+                  <TableCell align="right">{make.MakeId}</TableCell>
+                  <TableCell align="right">{make.MakeName}</TableCell>
+                  <TableCell align="right">Actions</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
 
-    return (
-        <p>Import Component</p>
-    )
-}
-
-export default Import
+export default Import;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,37 @@
-const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+const url =
+  "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json";
 
-export const addCar = (car) => {
-    return {
-        type: 'ADD_CAR',
-        value: car
-    }
-}
+export const addCar = car => {
+  return {
+    type: "ADD_CAR",
+    value: car
+  };
+};
 
-export const removeCar = (index) => {
-    return {
-        type: 'REMOVE_CAR',
-        value: index
-    }
-}
+export const removeCar = index => {
+  return {
+    type: "REMOVE_CAR",
+    value: index
+  };
+};
+
+export const fetchMakes = () => {
+  return dispatch => {
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        const action = {
+          type: "FETCH_MAKES",
+          value: response.Results
+        };
+        dispatch(action);
+      });
+  };
+};
+
+export const deleteMake = index => {
+  return {
+    type: "DELETE_MAKE",
+    value: index
+  };
+};
